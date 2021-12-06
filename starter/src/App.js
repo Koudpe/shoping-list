4c595d5963351ab528eb43319f07bcff1e169c38
Petr Koudelka <koudy@Petr-MacBook-Pro.local>
2021-12-06 07:59:59 +0100

    init

--- starter/src/App.js
+++ starter/src/App.js
@@ -5,43 +5,104 @@ import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '
 
 const App = () => {
 	// HINT: each "item" in our list names a name, a boolean to tell if its been completed, and a quantity
-	const [items, setItems] = useState([]);
+	const [items, setItems] = useState([
+		{itemName: "item 1", quantity: 1, isSelected: false},
+		{itemName: "item 2", quantity: 3, isSelected: false},
+		{itemName: "item 3", quantity: 2, isSelected: false},
+	]);
+
+	const [inputValue, setInputValue] = useState("");
+	const [totalItemCount, setTotalItemCount] = useState(0);
+
+	const handleAddButtonClick = () => {
+		// alert("click")
+		const newItem = {
+			itemName: inputValue,
+			quantity: 1,
+			isSelected: false,
+		}
+
+		const newItems = [...items, newItem];
+
+		setItems(newItems);
+		setInputValue('')
+	}
+
+	const handleQuantityIncrease = (index) => {
+		console.log("increase", index);
+
+		const newItems = [...items];
+
+		newItems[index].quantity++;
+
+		setItems(newItems);
+		calculateTotal();
+	}
+
+	const handleQuantityDecrease = (index) => {
+		console.log("decrease", index);
+
+		const newItems = [...items];
+
+		newItems[index].quantity--;
+
+		setItems(newItems);
+		calculateTotal();
+	}
+
+	const toggleComplete = (index) => {
+		const newItems = [...items];
+
+		newItems[index].isSelected = !newItems[index].isSelected;
+
+		setItems(newItems);
+	}
+
+	const calculateTotal = () => {
+		const totalItemCount = items.reduce((total, item) => {
+			return total + item.quantity;
+		}, 0);
+
+		setTotalItemCount(totalItemCount)
+	}
 
 	return (
 		<div className='app-background'>
 			<div className='main-container'>
 				<div className='add-item-box'>
-					<input className='add-item-input' placeholder='Add an item...' />
-					<FontAwesomeIcon icon={faPlus} />
+					<input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input' placeholder='Add an item...' />
+					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()}/>
 				</div>
 				<div className='item-list'>
+					{items.map((item, index) => (
 					<div className='item-container'>
-						<div className='item-name'>
+						<div className='item-name' onClick={()=> toggleComplete(index)}>
 							{/* HINT: replace false with a boolean indicating the item has been completed or not */}
-							{false ? (
+							{item.isSelected ? (
 								<>
 									<FontAwesomeIcon icon={faCheckCircle} />
-									<span className='completed'>Item 1</span>
+									<span className='completed'>{item.itemName}</span>
 								</>
 							) : (
 								<>
 									<FontAwesomeIcon icon={faCircle} />
-									<span>Item 1</span>
+									<span>{item.itemName}</span>
 								</>
 							)}
 						</div>
 						<div className='quantity'>
 							<button>
-								<FontAwesomeIcon icon={faChevronLeft} />
+								<FontAwesomeIcon icon={faChevronLeft} onClick={() => handleQuantityDecrease(index)}/>
 							</button>
-							<span> 1 </span>
+							<span>{item.quantity}</span>
 							<button>
-								<FontAwesomeIcon icon={faChevronRight} />
+								<FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityIncrease(index)}/>
 							</button>
 						</div>
 					</div>
+					))}
 				</div>
-				<div className='total'>Total: 6</div>
+				<div className='total'>Total: {totalItemCount}</div>
 			</div>
 		</div>
 	);
